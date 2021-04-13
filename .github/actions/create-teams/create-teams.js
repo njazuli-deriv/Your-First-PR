19d20978b16c2aa1698804df90baa3e67a65d409
Aaron Imming <aaim@protonmail.com>
2021-04-13 11:53:17 +0800

    Updates

---
+++ .github/actions/create-teams/create-teams.js
@@ -0,0 +1,23 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+const { Octokit } = require("@octokit/action");
+const { GITHUB_ACTIONS_BOT_NAME } = require("../utils");
+const octokit = new Octokit();
+const fs = require("fs");
+
+const createTeams = async () => {
+  try {
+    const team_size = core.getInput("team_size");
+    const testFolder = ".";
+
+    console.log({ cwd: process.cwd() });
+
+    fs.readdir(testFolder, (err, files) => {
+      files.forEach((file) => {
+        console.log(file);
+      });
+    });
+  } catch (error) {}
+};
+
+createTeams(github.context.payload.number).catch((e) => core.setFailed(e));
