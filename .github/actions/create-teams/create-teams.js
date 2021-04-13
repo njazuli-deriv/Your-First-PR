8419763322820d1c88756d8c349c627caa7a4860
Aaron Imming <aaim@protonmail.com>
2021-04-13 13:16:26 +0800

    Updates

--- .github/actions/create-teams/create-teams.js
+++ .github/actions/create-teams/create-teams.js
@@ -7,36 +7,38 @@ const fs = require("fs");
 
 const createTeams = async () => {
   try {
+    const people_folder_path = `${process.cwd()}/people`;
+    const teams_file_path = `${process.cwd()}/.very_secret_folder/teams.json`;
     const team_size = core.getInput("team_size");
-    const people_folder = `${process.cwd()}/people`;
-    const groups = fs
-      .readdirSync(people_folder)
+
+    // Get existing teams
+    const teams_file = fs.readFileSync(teams_file_path);
+    const existing_teams = JSON.parse(teams_file).groups;
+    const teams = fs
+      .readdirSync(people_folder_path)
       // .filter((person) => person !== GITHUB_ACTIONS_BOT_NAME)
-      .reduce(
-        (groups, person) => {
-          let current_group_idx = groups.findIndex(
-            (group) => group.length < team_size
-          );
+      .reduce((teams, person) => {
+        let current_team_idx = teams.findIndex(
+          (team) => team.length < team_size
+        );
 
-          // If no available group, create a new one.
-          if (current_group_idx === -1) {
-            current_group_idx = groups.length;
-          }
+        // If no available team, create a new one.
+        if (current_team_idx === -1) {
+          current_team_idx = teams.length;
+        }
 
-          // Add the person to this group.
-          groups[current_group_idx] = [
-            ...(Array.isArray(groups[current_group_idx])
-              ? groups[current_group_idx]
-              : []),
-            person,
-          ];
+        // Add the person to this team.
+        teams[current_team_idx] = [
+          ...(Array.isArray(teams[current_team_idx])
+            ? teams[current_team_idx]
+            : []),
+          person,
+        ];
 
-          return groups;
-        },
-        [[]]
-      );
+        return teams;
+      }, existing_teams);
 
-    console.log({ groups });
+    console.log({ teams });
   } catch {}
 };
 
--- .very_secret_folder/dist/create-teams/index.js
+++ .very_secret_folder/dist/create-teams/index.js
@@ -14,36 +14,38 @@ const fs = __nccwpck_require__(747);
 
 const createTeams = async () => {
   try {
+    const people_folder_path = __nccwpck_require__.ab + "people";
+    const teams_file_path = __nccwpck_require__.ab + "teams.json";
     const team_size = core.getInput("team_size");
-    const people_folder = __nccwpck_require__.ab + "people";
-    const groups = fs
+
+    // Get existing teams
+    const teams_file = fs.readFileSync(__nccwpck_require__.ab + "teams.json");
+    const existing_teams = JSON.parse(teams_file).groups;
+    const teams = fs
       .readdirSync(__nccwpck_require__.ab + "people")
       // .filter((person) => person !== GITHUB_ACTIONS_BOT_NAME)
-      .reduce(
-        (groups, person) => {
-          let current_group_idx = groups.findIndex(
-            (group) => group.length < team_size
-          );
+      .reduce((teams, person) => {
+        let current_team_idx = teams.findIndex(
+          (team) => team.length < team_size
+        );
 
-          // If no available group, create a new one.
-          if (current_group_idx === -1) {
-            current_group_idx = groups.length;
-          }
+        // If no available team, create a new one.
+        if (current_team_idx === -1) {
+          current_team_idx = teams.length;
+        }
 
-          // Add the person to this group.
-          groups[current_group_idx] = [
-            ...(Array.isArray(groups[current_group_idx])
-              ? groups[current_group_idx]
-              : []),
-            person,
-          ];
+        // Add the person to this team.
+        teams[current_team_idx] = [
+          ...(Array.isArray(teams[current_team_idx])
+            ? teams[current_team_idx]
+            : []),
+          person,
+        ];
 
-          return groups;
-        },
-        [[]]
-      );
+        return teams;
+      }, existing_teams);
 
-    console.log({ groups });
+    console.log({ teams });
   } catch {}
 };
 
